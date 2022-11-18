e55c6cb751dc02cf32421e558e9a63018b8c7fa4
Bharath <bhar6087@STTHOMAS.EDU>
2022-11-18 10:23:59 -0600

    Original files for project

---
+++ server.js
@@ -0,0 +1,99 @@
+// Built-in Node.js modules
+let fs = require('fs');
+let path = require('path');
+
+// NPM modules
+let express = require('express');
+let sqlite3 = require('sqlite3');
+
+
+let db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
+
+let app = express();
+let port = 8000;
+
+app.use(express.json());
+
+// Open SQLite3 database (in read-only mode)
+let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log('Error opening ' + path.basename(db_filename));
+    }
+    else {
+        console.log('Now connected to ' + path.basename(db_filename));
+    }
+});
+
+//EXAMPLE query
+//SELECT * FROM incidents WHERE date(date_time) >= '2022-05-29' AND code = 600 ORDER BY date_time DESC;
+//Selects all incidents for dates past 5/29/2022 with code 600 in descending order. 
+
+// GET request handler for crime codes
+app.get('/codes', (req, res) => {
+    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    
+    res.status(200).type('json').send({}); // <-- you will need to change this
+});
+
+// GET request handler for neighborhoods
+app.get('/neighborhoods', (req, res) => {
+    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    
+    res.status(200).type('json').send({}); // <-- you will need to change this
+});
+
+// GET request handler for crime incidents
+app.get('/incidents', (req, res) => {
+    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    
+    res.status(200).type('json').send({}); // <-- you will need to change this
+});
+
+// PUT request handler for new crime incident
+app.put('/new-incident', (req, res) => {
+    console.log(req.body); // uploaded data
+    
+    res.status(200).type('txt').send('OK'); // <-- you may need to change this
+});
+
+// DELETE request handler for new crime incident
+app.delete('/remove-incident', (req, res) => {
+    console.log(req.body); // uploaded data
+    
+    res.status(200).type('txt').send('OK'); // <-- you may need to change this
+});
+
+
+// Create Promise for SQLite3 database SELECT query 
+function databaseSelect(query, params) {
+    return new Promise((resolve, reject) => {
+        db.all(query, params, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(rows);
+            }
+        })
+    })
+}
+
+// Create Promise for SQLite3 database INSERT or DELETE query
+function databaseRun(query, params) {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, (err) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
+    })
+}
+
+
+// Start server - listen for client connections
+app.listen(port, () => {
+    console.log('Now listening on port ' + port);
+});
